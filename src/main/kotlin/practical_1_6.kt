d840a34c83bbcd46e7142a4ef38fc442dc39a23e
student <student@gnu.ac.in>
2023-07-20 07:56:17 +0530

    practical1

---
+++ src/main/kotlin/practical_1_6.kt
@@ -0,0 +1,15 @@
+fun main(){
+    println("bhoomi patel !! 21012011069")
+    var a= 111
+    var b= 2222
+    var c= -222
+    var ans = a+b+c
+    var ans1 = a-b-c
+    var ans2 = a*b*c
+    var ans3 = b/a
+    println("addition of $a $b $c is $ans")
+    println("subtraction of $a $b $c is $ans1")
+    println("multiplication of $a $b $c is $ans2")
+    println("division of  $b $c is $ans3")
+
+}
